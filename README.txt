c2169a45762fe7bc0c0057a7c244634d9c97eace
Gajh <khadimoullah.vencatasamy@ensta-bretagne.org>
2016-02-01 10:31:48 +0100

    Commit version 0.2

--- README.txt
+++ README.txt
@@ -1,8 +1,9 @@
 Objectives
 
-The system has to check if a flash drive is plugged, in that case the system crypts the flash drive then unmount it and a LED blink to signal that the device can be unplugged.
+The system has to check if a flash drive is plugged, in that case the system crypts the flash drive then unmount it and a LED blink to signal that the device can be unplugged. 
+In this version, a LED blink slowly when there is no flash drive plugged into the Armadeus, that means that your system is searching if a flash drive is plugged.
 
-In version 0.1 we do not crypt the flash drive.
+In version 0.2 we do not crypt the flash drive.
 
 To run:
 
--- test.sh
+++ test.sh
@@ -1,19 +1,40 @@
 #! /bin/sh
 GPIO_LINUX=21
 i=0
+i=2000000
+if [ -d /sys/class/gpio/gpio$GPIO_LINUX ]
+	then echo "gpio$GPIO_LINUX exists"
+else
+ 	echo $GPIO_LINUX > /sys/class/gpio/export
+fi
+
+echo "out" > /sys/class/gpio/gpio$GPIO_LINUX/direction
 while true; do
-	sleep 3
-	if [ -b /dev/sda ]
-		then	echo "mount"
+ 	#sleep 3
+ 	echo 1 > /sys/class/gpio/gpio$GPIO_LINUX/value 
+	sleep 5
+	usleep $i
+	echo 0 > /sys/class/gpio/gpio$GPIO_LINUX/value 
+	usleep $i
+
+
+	echo 1 > /sys/class/gpio/gpio$GPIO_LINUX/value 
+	usleep $i
+	echo 0 > /sys/class/gpio/gpio$GPIO_LINUX/value 
+	usleep $i
+
+	echo 1 > /sys/class/gpio/gpio$GPIO_LINUX/value 
+	usleep $i
+ 	echo 0 > /sys/class/gpio/gpio$GPIO_LINUX/value 
+	sleep 5
+	usleep $i
+ 	if [ -b /dev/sda ]
+ 		then	echo "mount"
 		mount /dev/sda1 /mnt/
 		sleep 1 
 		ls /mnt
-		umount /mnt
-		if [ -d /sys/class/gpio/gpio$GPIO_LINUX ]
-		then echo "gpio$GPIO_LINUX exists"
-		else
-			echo $GPIO_LINUX > /sys/class/gpio/export
-		fi
+		mount /dev/sda1 /mnt/ 
+ 		umount /mnt
 		echo "out" > /sys/class/gpio/gpio$GPIO_LINUX/direction
 		while [ $i -le 2 ];do
 			echo 1 > /sys/class/gpio/gpio$GPIO_LINUX/value 
@@ -23,8 +44,11 @@ while true; do
 			echo $i
 			i=$(($i+1))
 		done
-	else
+		i = 500000
+ 	else
 		echo "not ok"
 		i=0
-	fi
+		echo "no usb "
+		i=2000000
+ 	fi
 done
